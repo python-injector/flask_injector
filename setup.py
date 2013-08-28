a8490848bc43298344271214aad1f9e45293876f
Jakub Stasiak <jakub@stasiak.at>
2013-08-28 23:54:46 +0100

    Refactor setup script a bit

--- setup.py
+++ setup.py
@@ -3,22 +3,27 @@ from setuptools import setup
 
 long_description = open('README.rst').read()
 
-setup(
-    name='Flask-Injector',
-    version='0.3.0',
-    url='https://github.com/alecthomas/flask_injector',
-    license='BSD',
-    author='Alec Thomas',
-    author_email='alec@swapoff.org',
-    description='Adds Injector (dependency injection framework) support to Flask.',
-    long_description=long_description,
-    py_modules=['flask_injector'],
-    zip_safe=True,
-    platforms='any',
-    install_requires=[
-        'Flask', 'injector>=0.7.4',
-    ],
-    keywords=[
-        'Dependency Injection', 'DI', 'Flask',
-    ],
+# very convoluted way of reading version from the module without importing it
+version = (
+    [l for l in open('flask_injector.py') if '__version__' in l][0]
+    .split('=')[-1]
+    .strip().strip('\'')
 )
+
+if __name__ == '__main__':
+    setup(
+        name='Flask-Injector',
+        version=version,
+        url='https://github.com/alecthomas/flask_injector',
+        license='BSD',
+        author='Alec Thomas',
+        author_email='alec@swapoff.org',
+        description=
+            'Adds Injector, a Dependency Injection framework, support to Flask.',
+        long_description=long_description,
+        py_modules=['flask_injector'],
+        zip_safe=True,
+        platforms='any',
+        install_requires=['Flask', 'injector>=0.7.4'],
+        keywords=['Dependency Injection', 'Flask'],
+    )
