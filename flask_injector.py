7dd448cfcde83fe23a1a3109e2cfbc8dee94e9c4
Jakub Stasiak <jakub@stasiak.at>
2015-05-20 14:24:00 +0200

    Bump version to 0.6.0c1

--- flask_injector.py
+++ flask_injector.py
@@ -22,7 +22,7 @@ from injector import Module, Provider, Scope, ScopeDecorator, singleton
 
 
 __author__ = 'Alec Thomas <alec@swapoff.org>'
-__version__ = '0.5.0'
+__version__ = '0.6.0c1'
 __all__ = ['request', 'RequestScope', 'Config', 'Request', 'FlaskInjector', ]
 
 
