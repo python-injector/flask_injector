277d5d77bc29650275b14fbe1a27bbee982d6321
Jakub Stasiak <jakub@stasiak.at>
2015-05-20 21:28:50 +0200

    Stop lying about Python 3.2 support

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.2',
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: Implementation :: CPython',
