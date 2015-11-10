python-injector/flask_injector

commit 91a11254f67ac14ea935fa350c94cbe15df8ed25

Add Flask-RestPlus integration
This feels like a hack but hey - it is one. It seems to be working.

Closes GitHub #13

flask_injector.py
@@ -83,6 +83,24 @@ def wrap_class_based_view(fun, injector):
         flask_restplus and
         isinstance(flask_restful_api, flask_restplus.Api)
     ):
+        # This is flask_restplus' add_resource implementation:
+        #
+        #     def add_resource(self, resource, *urls, **kwargs):
+        #     (...)
+        #     args = kwargs.pop('resource_class_args', [])
+        #     if isinstance(args, tuple):
+        #         args = list(args)
+        #     args.insert(0, self)
+        #     kwargs['resource_class_args'] = args
+        #
+        #     super(Api, self).add_resource(resource, *urls, **kwargs)
+        #
+        # Correspondingly, flask_restplus.Resource's constructor expects
+        # flask_restplus.Api instance in its first argument; since we detected
+        # that we're very likely dealing with flask_restplus we'll provide the Api
+        # instance as keyword argument instead (it'll work just as well unless
+        # flask_restplus changes the name of the parameter; also
+        # Injector.create_object doesn't support extra positional arguments anyway).
         if 'api' in class_kwargs:
             raise AssertionError('api keyword argument is reserved')
 

.travis.yml
@@ -9,7 +9,7 @@ env:
   - INJECTOR_VERSION="==0.9.0"
   - INJECTOR_VERSION=""
 install:
-  - pip install flake8 nose==1.3.0 flask==0.10.1 flask_restful injector$INJECTOR_VERSION flask_cache flask_sqlalchemy eventlet
+  - pip install flake8 nose==1.3.0 flask==0.10.1 flask_restful flask_restplus injector$INJECTOR_VERSION flask_cache flask_sqlalchemy eventlet
 script:
   - flake8 --max-line-length=110 *.py
   - nosetests -v

flask_injector_tests.py
@@ -3,6 +3,7 @@ import json
 import warnings
 
 import flask_restful
+import flask_restplus
 from eventlet import greenthread
 from injector import CallableProvider, inject
 from flask import Blueprint, Flask
@@ -303,6 +304,25 @@ def test_flask_restful_integration_works():
 
     client = app.test_client()
     response = client.get('/')
+    data = json.loads(response.data.decode('utf-8'))
+    eq_(data, {'int': 0})
+
+
+def test_flask_restplus_integration_works():
+    @inject(_int=int)
+    class HelloWorld(flask_restplus.Resource):
+        def get(self):
+            return {'int': self._int}
+
+    app = Flask(__name__)
+    api = flask_restplus.Api(app)
+
+    api.add_resource(HelloWorld, '/hello')
+
+    FlaskInjector(app=app)
+
+    client = app.test_client()
+    response = client.get('/hello')
     data = json.loads(response.data.decode('utf-8'))
     eq_(data, {'int': 0})
 

commit 9c5b8e822703fc28953142d0b610fd2e0868fe3e

Fix the whitespace
Fixes: 91a11254f67ac14ea935fa350c94cbe15df8ed25

flask_injector.py
@@ -139,6 +139,7 @@ def wrap_class_based_view(fun, injector):
 
     return fun
 
+
 def wrap_flask_restful_resource(fun, flask_restful_api, injector):
     """
     This is needed because of how flask_restful views are registered originally.