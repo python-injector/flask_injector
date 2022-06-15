python-injector/flask_injector

commit 4d8bfe8ff55d8f1e164f9b9aba4c82dcb57eb9cb

(wip) fix: name 'Response' is not defined
Aim to resolve #69

flask_injector/__init__.py
@@ -62,15 +62,13 @@ def wrap_fun(fun: T, injector: Injector) -> T:
     if hasattr(fun, '__call__') and not isinstance(fun, type):
         try:
             type_hints = get_type_hints(fun)
-        except (AttributeError, TypeError):
+        except (AttributeError, TypeError, NameError):
             # Some callables aren't introspectable with get_type_hints,
             # let's assume they don't have anything to inject. The exception
             # types handled here are what I encountered so far.
             # It used to be AttributeError, then https://github.com/python/typing/pull/314
             # changed it to TypeError.
             wrap_it = False
-        except NameError:
-            wrap_it = True
         else:
             type_hints.pop('return', None)
             wrap_it = type_hints != {}

commit 993c0bbe5063bef511da60d8b25383a498a59f09

Fix all Flask compatibility issues and make the build green (#75)
Thanks to injector 0.20.0 invalid forward references in return type
annotation positions no longer break injection/detecting dependencies
(which is what GH-69 is actually about).

This PR fixes https://github.com/alecthomas/flask_injector/issues/69

This PR also effectively reverts [1] as [1] was breaking forward
reference support.

One more thing being fixed here is class-based view failures
that started happening for some reason, reordering some internal
logic handles that (we prioritize treating views as class-based views
and all is well).

[1] Fixes: 4d8bfe8ff55d ("(wip) fix: name 'Response' is not defined")

flask_injector/__init__.py
@@ -59,24 +59,26 @@ def wrap_fun(fun: T, injector: Injector) -> T:
     if hasattr(fun, '__bindings__'):
         return wrap_function(fun, injector)
 
+    if hasattr(fun, 'view_class'):
+        return wrap_class_based_view(fun, injector)
+
     if hasattr(fun, '__call__') and not isinstance(fun, type):
         try:
             type_hints = get_type_hints(fun)
-        except (AttributeError, TypeError, NameError):
+        except (AttributeError, TypeError):
             # Some callables aren't introspectable with get_type_hints,
             # let's assume they don't have anything to inject. The exception
             # types handled here are what I encountered so far.
             # It used to be AttributeError, then https://github.com/python/typing/pull/314
             # changed it to TypeError.
             wrap_it = False
+        except NameError:
+            wrap_it = True
         else:
             type_hints.pop('return', None)
             wrap_it = type_hints != {}
         if wrap_it:
             return wrap_fun(inject(fun), injector)
-
-    if hasattr(fun, 'view_class'):
-        return wrap_class_based_view(fun, injector)
 
     return fun
 

setup.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         package_data={'flask_injector': ['py.typed']},
         zip_safe=True,
         platforms='any',
-        install_requires=['Flask>=2.1.2', 'injector>=0.10.0', 'typing; python_version < "3.5"'],
+        install_requires=['Flask>=2.1.2', 'injector>=0.20.0', 'typing; python_version < "3.5"'],
         keywords=['Dependency Injection', 'Flask'],
         classifiers=[
             'Development Status :: 5 - Production/Stable',